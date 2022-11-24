78a8f63842b2f6fb41de9991a34ad015f445051b
Nikita Vorontsov <vorontsov.nstu@yandex.ru>
2022-11-24 12:00:17 +0700

    fixes

--- init.el
+++ init.el
@@ -37,7 +37,9 @@
  '(Info-default-directory-list
    '("/opt/homebrew/Cellar/emacs-plus@28/28.2/share/info/emacs/" "/usr/share/info/" "/opt/homebrew/share/info/"))
  '(c-basic-offset 4)
- '(custom-enabled-themes nil)
+ '(custom-enabled-themes '(modus-operandi))
+ '(custom-safe-themes
+   '("c0f4b66aa26aa3fded1cbefe50184a08f5132756523b640f68f3e54fd5f584bd" default))
  '(display-time-mode t)
  '(erlang-new-clause-with-arguments t)
  '(fill-column 120)
@@ -45,7 +47,9 @@
  '(lsp-ui-doc-enable nil)
  '(lsp-ui-sideline-enable nil)
  '(package-selected-packages
-   '(org-roam flycheck projectile-ripgrep counsel utop tuareg company-web company-erlang eldoc-box exunit elixir-mode mix eglot macrostep-geiser geiser-guile company racket-mode htmlize js2-mode vlf zerodark-theme web-mode expand-region geiser projectile slime smartparens erlang magit rust-mode cargo haskell-mode lsp-mode flymake lsp-ui company-mode lsp-treemacs lsp-ivy))
+   '(erlang org-roam flycheck projectile-ripgrep counsel utop tuareg company-web company-erlang eldoc-box exunit elixir-mode mix eglot macrostep-geiser geiser-guile company racket-mode htmlize js2-mode vlf zerodark-theme web-mode expand-region geiser projectile slime smartparens magit rust-mode cargo haskell-mode lsp-mode flymake lsp-ui company-mode lsp-treemacs lsp-ivy))
+ '(projectile-ignored-projects
+   '("~/Projects/ug/ugos-7.1.0/ugos/package/utm-core/" "~/Projects/ug/ugos-7.0.2/ugos/package/utm-core/" "~/Projects/ug/ugos-7.0.1/ugos/package/utm-core/" "~/Projects/ug/branch-6.0.3/utm/"))
  '(projectile-tags-backend 'etags-select)
  '(projectile-tags-command "find -name *.[he]rl -print | etags -")
  '(safe-local-variable-values '((org-image-actual-width quote true)))
@@ -63,6 +67,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;======= Some extra config and utils ================
+(add-to-list 'load-path "~/.emacs.d/extra/")
+(require 'ug)
 
 ;=======Shortcuts==============================
 (global-set-key (kbd "C-.") 'undo)
@@ -124,13 +132,13 @@
 
 ;------Erlang--------------------------------
 ;; doc: http://erlang.org/doc/apps/tools/erlang_mode_chapter.html
-(cond ((file-exists-p "/usr/lib/erlang/erts-12.2.1/")
-       (setq erlang-root-dir "/usr/lib/erlang/erts-12.2.1/"))
+(cond ((file-exists-p "/usr/lib/erlang/erts-12.2.1")
+       (setq erlang-root-dir "/usr/lib/erlang/erts-12.2.1"))
       ;; ((file-exists-p (expand-file-name "~/.guix-profile/lib/erlang/erts-11.0.3/"))
       ;;  (setq erlang-root-dir (expand-file-name "~/.guix-profile/lib/erlang/erts-11.0.3/")))
       )
 
-(maybe-add-to-load-path "/usr/lib/erlang/lib/tools-3.5.2/emacs/")
+;(maybe-add-to-load-path "/usr/lib/erlang/lib/tools-3.5.2/emacs")
 (maybe-add-to-exec-path "/usr/lib/erlang/bin")
 ;; (maybe-add-to-load-path "~/.guix-profile/lib/erlang/lib/tools-3.3/emacs/")
 ;; (maybe-add-to-exec-path "~/.guix-profile/lib/erlang/bin/")
@@ -216,8 +224,8 @@
   (if (not indent-tabs-mode)
       (untabify (point-min) (point-max)))
   nil)
-(add-to-list 'write-file-functions 'untabify-current-buffer)
-(add-to-list 'write-file-functions 'delete-trailing-whitespace)
+;; (add-to-list 'write-file-functions 'untabify-current-buffer)
+;; (add-to-list 'write-file-functions 'delete-trailing-whitespace)
 
 ;; (setq geiser-active-implementations '(guile))
 ;(require 'geiser)                       ;minor scheme mode
@@ -264,6 +272,7 @@
 ;; https://github.com/BurntSushi/ripgrep
 ;; sudo apt install fd-find ripgrep
 (projectile-mode +1)
+;(projectile-discover-projects-in-search-path)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
@@ -271,7 +280,4 @@
 (setq revert-without-query '("TAGS"))
 (setq projectile-completion-system 'ivy)
 ;(add-hook 'projectile-idle-timer-hook #'foo)  ; run this hook every 30 sec (by default there TAGS rebuild)
-(setq projectile-project-search-path '(("~/Projects/" . 1)))
 
-(add-to-list 'load-path "~/.emacs.d/extra/")
-(require 'ug)
