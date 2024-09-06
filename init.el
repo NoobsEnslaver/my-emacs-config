9cf04229e5638c5350df0ac8e27f39fa18e99440
Nikita Vorontsov <vorontsov.nstu@yandex.ru>
2024-09-06 21:48:43 +0300

    remove not used libs

--- init.el
+++ init.el
@@ -21,8 +21,6 @@
   (maybe-add-to-list path 'exec-path))
 
 ;; ---------- Init -----------------------------------
-;(package-initialize)
-
 (require 'package)
 (add-to-list 'package-archives
              '("org" . "https://orgmode.org/elpa/") t)
@@ -35,29 +33,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(c-basic-offset 4)
- '(connection-local-criteria-alist
-   '(((:application tramp :protocol "flatpak")
-      tramp-container-connection-local-default-flatpak-profile)
-     ((:application tramp)
-      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
  '(custom-enabled-themes '(modus-vivendi))
- '(custom-safe-themes
-   '("1d2e7f3afdd436cf7b1f7d009111e9890328da1f68380c71ad8041ebd62a0a95" "c0f4b66aa26aa3fded1cbefe50184a08f5132756523b640f68f3e54fd5f584bd" default))
  '(default-input-method "russian-computer")
  '(erlang-new-clause-with-arguments t)
  '(fill-column 120)
  '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
- '(ispell-dictionary nil)
  '(package-selected-packages
-   '(go-errcheck go-scratch go-tag gotest go-mode go-projectile go-snippets sly yasnippet jsonrpc julia-ts-mode eglot project julia-repl julia-formatter eglot-jl erlang flycheck projectile-ripgrep counsel utop tuareg company-web company-erlang eldoc-box exunit elixir-mode mix macrostep-geiser geiser-guile company racket-mode htmlize js2-mode vlf zerodark-theme web-mode expand-region geiser projectile smartparens magit rust-mode cargo haskell-mode flymake company-mode))
+   '(go-errcheck go-scratch go-tag gotest go-mode go-projectile go-snippets sly yasnippet jsonrpc project erlang flycheck projectile-ripgrep counsel eldoc-box company htmlize js2-mode vlf zerodark-theme web-mode expand-region projectile smartparens magit haskell-mode flymake))
  '(projectile-project-search-path '(("~/Projects/" . 1)))
- '(safe-local-variable-values
-   '((Package . CL-USER)
-     (Base . 10)
-     (Package . HUNCHENTOOT)
-     (Syntax . COMMON-LISP)
-     (org-image-actual-width quote true)))
  '(sgml-basic-offset 4)
  '(standard-indent 4)
  '(tab-always-indent nil)
@@ -84,8 +68,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Anonymous Pro" :foundry "mlss" :slant normal :weight regular :height 102 :width normal)))))
-
+ '(default ((t (:family "Anonymous Pro" :foundry "mlss" :slant normal :weight regular :height 151 :width normal)))))
 
 
 ;; ----------- Tree sitter ---------------
@@ -105,7 +88,10 @@
      (haskell   "https://github.com/tree-sitter/tree-sitter-haskell")
      (julia     "https://github.com/tree-sitter/tree-sitter-julia")
      (rust      "https://github.com/tree-sitter/tree-sitter-rust")
-     (go        "https://github.com/tree-sitter/tree-sitter-go")))
+     (go        "https://github.com/tree-sitter/tree-sitter-go")
+     (c         "https://github.com/tree-sitter/tree-sitter-c")
+     (cpp       "https://github.com/tree-sitter/tree-sitter-cpp")
+     (yaml      "https://github.com/ikatyang/tree-sitter-yaml")))
 
 ;======= Some extra config and utils ================
 ;(add-to-list 'load-path "~/.emacs.d/extra/")
@@ -132,28 +118,13 @@
 ;------Smartparens---------------------------------
 ;; doc: https://github.com/Fuco1/smartparens
 (require 'smartparens)
+(require 'smartparens-config)
 (smartparens-global-mode 1)             ;Скобки ставятся парами во всех режимах
 
 ;------Magit---------------------------------
 ;; https://magit.vc/manual/magit.html
 (global-set-key (kbd "C-c m d") 'magit-dispatch)
 (global-set-key (kbd "C-c m s") 'magit-status)
-
-;----------Elixir---------------
-(require 'eglot)
-;; (require 'exunit)
-;; (require 'eldoc-box)
-
-;(add-to-list 'eglot-server-programs
-;             '(elixir-mode "~/Projects/elixir-ls/release/language_server.sh"))
-;; (add-hook
-;;  'elixir-mode-hook
-;;  (lambda ()
-;;    (subword-mode)
-;;    (eglot-ensure)
-;;    (company-mode)
-;;    (flymake-mode)
-;;    (add-hook 'before-save-hook 'eglot-format nil t)))
 
 ;------Erlang--------------------------------
 ;; doc: http://erlang.org/doc/apps/tools/erlang_mode_chapter.html
@@ -168,46 +139,41 @@
 ;; (maybe-add-to-load-path "~/.guix-profile/lib/erlang/lib/tools-3.3/emacs/")
 ;; (maybe-add-to-exec-path "~/.guix-profile/lib/erlang/bin/")
 
-(require 'erlang-start)
+;(require 'erlang-start)
 ;(add-hook 'erlang-mode-hook 'company-mode)
-(add-hook 'erlang-mode-hook '(lambda() (setq indent-tabs-mode nil)))    ;использовать пробелы вместо табуляций
-
-;-----ORG-Jira-------------------------------
-;; doc: https://github.com/ahungry/org-jira
-;; echo 'machine your-site.atlassian.net login you@example.com password yourPassword port 80' > ~/.authinfo
-;; (setq jiralib-url "https://jira.ringcentral.com")
+;(add-hook 'erlang-mode-hook '(lambda() (setq indent-tabs-mode nil)))    ;использовать пробелы вместо табуляций
 
 ;------Java Script---------------------------
 ;; doc: https://github.com/mooz/js2-mode/
 ;(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode)) ;web-mode also supports
-(add-hook 'js-mode-hook 'js2-minor-mode)
-(setq js2-basic-offset 4)
+;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; (add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode)) ;web-mode also supports
+;; (add-hook 'js-mode-hook 'js2-minor-mode)
+;; (setq js2-basic-offset 4)
 
 ;; -------- View Large Files mode -------------------------
 ;; doc: https://github.com/m00natic/vlfi
 (require 'vlf-setup)                    ;ask to use mode when opening large file
 
 ;; -------- Org Mode --------------------------------------
-(require 'org)
-(require 'ox-latex)
-(add-to-list 'org-latex-packages-alist '("" "minted"))
-(setq org-latex-listings 'minted)
+;; (require 'org)
+;; (require 'ox-latex)
+;; (add-to-list 'org-latex-packages-alist '("" "minted"))
+;; (setq org-latex-listings 'minted)
 
-(setq org-latex-pdf-process
-      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+;; (setq org-latex-pdf-process
+;;       '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+;;         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+;;         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
-(setq org-src-fontify-natively t)
+;; (setq org-src-fontify-natively t)
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((R . t)
-   (latex . t)))
+;; (org-babel-do-load-languages
+;;  'org-babel-load-languages
+;;  '((R . t)
+;;    (latex . t)))
 
-(add-hook 'org-mode-hook 'auto-fill-mode)
+;; (add-hook 'org-mode-hook 'auto-fill-mode)
 
 ;========== Variables =====================================
 (global-display-line-numbers-mode 1)
@@ -238,53 +204,27 @@
 (add-to-list 'write-file-functions 'untabify-current-buffer)
 (add-to-list 'write-file-functions 'delete-trailing-whitespace)
 
-;; (setq geiser-active-implementations '(guile))
-;(require 'geiser)                       ;minor scheme mode
-(require 'smartparens-config)
-
-;; Rust
-;(require 'rust-mode)
-;(require 'cargo)
-;(add-hook 'rust-mode-hook 'cargo-minor-mode)
-;(add-hook 'rust-mode-hook 'auto-complete-mode)
 
 ;; Common Lisp
-(setq inferior-lisp-program "sbcl")
-(require 'sly-autoloads)
+;; (setq inferior-lisp-program "sbcl")
+;; (require 'sly-autoloads)
 
 ;; company
 ;; doc: http://company-mode.github.io/
-;(require 'company)
-;(require 'company-erlang)
+(require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
 ;; Haskell
-(add-hook 'haskell-mode-hook 'company-mode)
 (eval-after-load "haskell-mode"
     '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
 (eval-after-load "haskell-cabal"
     '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
 
-;; (use-package eglot
-;;   :ensure t
-;;   :config (add-hook 'haskell-mode-hook 'eglot-ensure)
-;;   :config (add-hook 'julia-mode-hook 'eglot-ensure)
-;;   ;; :config
-;;   (setq-default eglot-workspace-configuration
-;;                 '((haskell
-;;                    (plugin
-;;                     (stan
-;;                      (globalOn . :json-false))))))  ;; disable stan
-;;   :custom
-;;   ;; (eglot-autoshutdown t)  ;; shutdown language server after closing last file
-;;   (eglot-confirm-server-initiated-edits nil)  ;; allow edits without confirmation
-;;   )
-
 ;; Counsel (Ivy)
 ;; https://oremacs.com/swiper/
 (ivy-mode 1)
 ;(global-set-key (kbd "C-s") 'swiper-isearch)
-;(global-set-key (kbd "C-c k") 'counsel-rg)
+(global-set-key (kbd "C-c k") 'counsel-rg)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (setq ivy-use-virtual-buffers t)
@@ -303,15 +243,6 @@
 (setq revert-without-query '("TAGS"))
 (setq projectile-completion-system 'ivy)
 ;(add-hook 'projectile-idle-timer-hook #'foo)  ; run this hook every 30 sec (by default there TAGS rebuild)
-
-;; Julia
-;; (eglot-jl-init)
-
-;; при первом запуске лучше скомпилировать ls в julia-консоли
-;; julia --project=/home/ne/.emacs.d/elpa/eglot-jl-20230601.1335/ /home/ne/.emacs.d/elpa/eglot-jl-20230601.1335/eglot-jl.jl
-;(set 'eglot-connect-timeout 600)
-;(add-hook 'julia-mode-hook 'eglot-ensure)
-(maybe-add-to-exec-path "~/.juliaup/bin")
 
 (yas-global-mode)
 (maybe-add-to-exec-path "~/.local/bin)")
